71971a4f1325c1e2e4f9d6108fb778a10791015b
Luis Thiam-Nye <luis.tn@protonmail.ch>
2022-11-04 16:25:54 +0000

    receiving commands via terminal POC
    also fighting with storing wifi secrets, HOW?

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -8,12 +8,17 @@
 
 #include <SPI.h>
 #include <WiFiNINA.h>
+// #include <wifi_secrets.h>
 
 // Wifi
+// char wifi_ssid[] = WifiSecrets::SSID;
+// char wifi_pass[] = WifiSecrets::PASS;
 char wifi_ssid[] = "squirrel";
 char wifi_pass[] = "75/44xG1";
+
 int wifi_status = WL_IDLE_STATUS;
 WiFiServer wifi_server(23); // arg1 = server port
+bool is_client_connected = false;
 
 void setup_ap() {
   Serial.println("Starting web server for access point");
@@ -22,18 +27,6 @@ void setup_ap() {
     Serial.println("ERR: Could not communicate with Wi-Fi module");
     while (true); // STOP
   }
-
-  Serial.println("Starting access point");
-
-  // Access point
-  // ap_status = WiFi.beginAP(ap_ssid, ap_pass);
-  // if (ap_status != WL_AP_LISTENING) {
-  //   Serial.println("ERR: Could not start access point");
-  //   while (true); // STOP
-  // }
-  // // wait for AP connection
-  // delay(5);
-  // ap_server.begin();
 
   while (wifi_status != WL_CONNECTED) {
     Serial.print("Wifi: Connecting to SSID: ");
@@ -44,6 +37,7 @@ void setup_ap() {
   wifi_server.begin();
 
   IPAddress ip = WiFi.localIP();
+  Serial.println("Wifi: Connected from IP address:");
   Serial.println(ip);
 
 }
@@ -64,83 +58,28 @@ void tick_wifi() {
   // wait for a new client to connect
   WiFiClient client = wifi_server.available();
   if (client) {
-    // if (!alreadyConnected) {
-    //   // clead out the input buffer:
-    //   client.flush();
-    //   Serial.println("We have a new client");
-    //   client.println("Hello, client!");
-    //   alreadyConnected = true;
-    // }
+    // Clear input buffer if new connection
+    if (!is_client_connected) {
+      client.flush();
+      wifi_server.println("Welcome to the Fourth Wheel");
+      is_client_connected = true;
+    }
+
     if (client.available()) {
-      // then there bytes available to read from the client,
-      char thisChar = client.read();
-      // echo
-      wifi_server.write(thisChar);
-      // echo to server
-      Serial.write(thisChar);
+      // Then: there bytes available to read from the client,
+      char ch = client.read();
+      if (ch == 'f') {
+        wifi_server.println("Respects paid");
+      } else if (ch == '\r' || ch == '\n') {}
+      else {
+        wifi_server.print("Unknown command: ");
+        wifi_server.write(ch);
+        wifi_server.println();
+      }
     }
   }
 }
 
-// void tick_ap() {
-//   if (wifi_status != WiFi.status()) {
-//     // When status changed
-//     wifi_status = WiFi.status();
-//     if (wifi_status == WL_AP_CONNECTED) {
-//       Serial.println("AP: Device connected");
-//     } else {
-//       Serial.println("AP: Device disconnected, now listening");
-//     }
-//   }
-
-//   WiFiClient client = wifi_server.available();
-//   if (client) {
-//     Serial.println("AP: New client");
-//     String currentLine = "";
-//     while (client.connected()) {
-//       if (client.available()) {
-//         // then there bytes available to read from the client,
-//         char c = client.read(); // byte
-//         Serial.write(c);
-//         if (c == '\n') {
-//           if (currentLine.length() == 0) {
-//             // HTTP response
-//             client.println("HTTP/1.1 200 OK");
-//             client.println("Content-type:text/html");
-//             client.println();
-
-//             // Content
-//             client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
-//             client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
-
-//             client.println();
-//             break;
-//           }
-//           else {
-//             currentLine = "";
-//           }
-//         }
-//         else if (c != '\r') {
-//           currentLine += c;
-//         }
-
-//         // Actions
-//         if (currentLine.endsWith("GET /H")) {
-//           digitalWrite(LED_BUILTIN, HIGH);
-//         }
-//         if (currentLine.endsWith("GET /L")) {
-//           digitalWrite(LED_BUILTIN, LOW);
-//         }
-//       }
-//     }
-//     // close the connection:
-//     client.stop();
-//     Serial.println("client disconnected");
-//   }
-// }
-
 void loop() {
-  // Serial.println("Hello World");
   tick_wifi();
-  // delay(2000);
 }
